c1c0fa039319f91db097e346a1fcb73a421535f5
Nicolas Depelteau <nicolas@depelteau.ca>
2024-03-01 12:43:57 -0500

    anomalie fix long method

--- app/src/main/java/com/naman14/timber/utils/TimberUtils.java
+++ app/src/main/java/com/naman14/timber/utils/TimberUtils.java
@@ -390,20 +390,18 @@ public class TimberUtils {
             for (NetworkInterface intf : interfaces) {
                 List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
                 for (InetAddress addr : addrs) {
-                    if (!addr.isLoopbackAddress()) {
-                        String sAddr = addr.getHostAddress();
-                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
-                        boolean isIPv4 = sAddr.indexOf(':')<0;
+                    if (addr.isLoopbackAddress()) {
+                        continue;
+                    }
+                    String sAddr = addr.getHostAddress();
+                    //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                    boolean isIPv4 = sAddr.indexOf(':')<0;
 
-                        if (useIPv4) {
-                            if (isIPv4)
-                                return sAddr;
-                        } else {
-                            if (!isIPv4) {
-                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
-                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
-                            }
-                        }
+                    if (useIPv4 && isIPv4) {
+                        return sAddr;
+                    } else if (!isIPv4){
+                        int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                        return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
                     }
                 }
             }
